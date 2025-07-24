9291d74b369e6f37ed3c123c5559e0b7f775a51f
GaodaGG <zil0718@outlook.com>
2025-07-24 11:20:28 +0800

    update readme

--- src/main/java/com/gg/SaltFullPlugin/MainPlugin.java
+++ src/main/java/com/gg/SaltFullPlugin/MainPlugin.java
@@ -19,7 +19,7 @@ public class MainPlugin extends Plugin {
     public void start() {
         super.start();
         System.out.println("MainPlugin started");
-        ConsoleWindow.showConsole();
+//        ConsoleWindow.showConsole();
 
 //        new Thread(() -> {
 //            try {
